4be01877a66da47cd0d457a1a59855d85831932c
xuchengpu <1550540124@qq.com>
2017-01-06 18:15:48 +0800

    手机影音项目是真实的上线项目，本视频在原项目基础上，进行了新技术的更新和优化。该项目包括本地音乐播放、网络音乐播放、本地视频播放、网络直播四大模块，几乎涵盖了市面上视频播放类APP的全部技术。

---
+++ 我的手机影音/src/main/java/com/xuchengpu/myproject/MainActivity.java
@@ -0,0 +1,17 @@
+package com.xuchengpu.myproject;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by 许成谱 on 2017/1/6.
+ */
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+}
---
+++ 我的手机影音/src/main/java/com/xuchengpu/myproject/WelcomeActivity.java
@@ -0,0 +1,45 @@
+package com.xuchengpu.myproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+
+public class WelcomeActivity extends AppCompatActivity {
+    private Handler handler=new Handler();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+            startActivity();
+            }
+        },2000);
+
+    }
+
+    private void startActivity() {
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+        return super.onTouchEvent(event);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        handler.removeCallbacksAndMessages(null);
+    }
+}
