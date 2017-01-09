11afcbadfb421be7739c68518e025833059bccf2
xuchengpu <1550540124@qq.com>
2017-01-09 23:02:17 +0800

    实现播放上一个视频和下一个视频

--- 我的手机影音/src/main/java/com/xuchengpu/myproject/myproject/activity/SystemVideoPlayer.java
+++ 我的手机影音/src/main/java/com/xuchengpu/myproject/myproject/activity/SystemVideoPlayer.java
@@ -48,18 +48,18 @@ public class SystemVideoPlayer extends Activity implements View.OnClickListener,
     private Button btnSwichScreen;
     private Utils utils;
     private ArrayList<MediaItem> video;
-    private Handler handler=new Handler(){
+    private Handler handler = new Handler() {
         @Override
         public void handleMessage(Message msg) {
             super.handleMessage(msg);
             switch (msg.what) {
-                case  PROGRESS:
-                    int currentPosition=videoview.getCurrentPosition();
+                case PROGRESS:
+                    int currentPosition = videoview.getCurrentPosition();
                     tvCurrenttime.setText(utils.stringForTime(currentPosition));
                     seekbarVideo.setProgress(currentPosition);
                     tvSystetime.setText(getSystemTime());
                     handler.removeMessages(PROGRESS);
-                    handler.sendEmptyMessageDelayed(PROGRESS,1000);
+                    handler.sendEmptyMessageDelayed(PROGRESS, 1000);
                     break;
             }
         }
@@ -67,12 +67,12 @@ public class SystemVideoPlayer extends Activity implements View.OnClickListener,
     private int position;
 
     private String getSystemTime() {
-        SimpleDateFormat format=new SimpleDateFormat("HH:mm:ss");
-        String systemTime=format.format(new Date());
+        SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+        String systemTime = format.format(new Date());
         return systemTime;
     }
 
-    private final  static int PROGRESS=0;
+    private final static int PROGRESS = 0;
     private MyBatterReceiver receive;
 
 
@@ -84,30 +84,30 @@ public class SystemVideoPlayer extends Activity implements View.OnClickListener,
      */
     private void findViews() {
         videoview = (VideoView) findViewById(R.id.videoview);
-        llTop = (LinearLayout)findViewById( R.id.ll_top );
-        tvName = (TextView)findViewById( R.id.tv_name );
-        ivBattery = (ImageView)findViewById( R.id.iv_battery );
-        tvSystetime = (TextView)findViewById( R.id.tv_systetime );
-        btnVoice = (Button)findViewById( R.id.btn_voice );
-        seekbarVoice = (SeekBar)findViewById( R.id.seekbar_voice );
-        btnSwichePlayer = (Button)findViewById( R.id.btn_swiche_player );
-        llBottom = (LinearLayout)findViewById( R.id.ll_bottom );
-        tvCurrenttime = (TextView)findViewById( R.id.tv_currenttime );
-        seekbarVideo = (SeekBar)findViewById( R.id.seekbar_video );
-        tvDuration = (TextView)findViewById( R.id.tv_duration );
-        btnExit = (Button)findViewById( R.id.btn_exit );
-        btnPre = (Button)findViewById( R.id.btn_pre );
-        btnStartPause = (Button)findViewById( R.id.btn_start_pause );
-        btnNext = (Button)findViewById( R.id.btn_next );
-        btnSwichScreen = (Button)findViewById( R.id.btn_swich_screen );
+        llTop = (LinearLayout) findViewById(R.id.ll_top);
+        tvName = (TextView) findViewById(R.id.tv_name);
+        ivBattery = (ImageView) findViewById(R.id.iv_battery);
+        tvSystetime = (TextView) findViewById(R.id.tv_systetime);
+        btnVoice = (Button) findViewById(R.id.btn_voice);
+        seekbarVoice = (SeekBar) findViewById(R.id.seekbar_voice);
+        btnSwichePlayer = (Button) findViewById(R.id.btn_swiche_player);
+        llBottom = (LinearLayout) findViewById(R.id.ll_bottom);
+        tvCurrenttime = (TextView) findViewById(R.id.tv_currenttime);
+        seekbarVideo = (SeekBar) findViewById(R.id.seekbar_video);
+        tvDuration = (TextView) findViewById(R.id.tv_duration);
+        btnExit = (Button) findViewById(R.id.btn_exit);
+        btnPre = (Button) findViewById(R.id.btn_pre);
+        btnStartPause = (Button) findViewById(R.id.btn_start_pause);
+        btnNext = (Button) findViewById(R.id.btn_next);
+        btnSwichScreen = (Button) findViewById(R.id.btn_swich_screen);
 
-        btnVoice.setOnClickListener( this );
-        btnSwichePlayer.setOnClickListener( this );
-        btnExit.setOnClickListener( this );
-        btnPre.setOnClickListener( this );
-        btnStartPause.setOnClickListener( this );
-        btnNext.setOnClickListener( this );
-        btnSwichScreen.setOnClickListener( this );
+        btnVoice.setOnClickListener(this);
+        btnSwichePlayer.setOnClickListener(this);
+        btnExit.setOnClickListener(this);
+        btnPre.setOnClickListener(this);
+        btnStartPause.setOnClickListener(this);
+        btnNext.setOnClickListener(this);
+        btnSwichScreen.setOnClickListener(this);
     }
 
     /**
@@ -118,30 +118,34 @@ public class SystemVideoPlayer extends Activity implements View.OnClickListener,
      */
     @Override
     public void onClick(View v) {
-        if ( v == btnVoice ) {
+        if (v == btnVoice) {
             // Handle clicks for btnVoice
-        } else if ( v == btnSwichePlayer ) {
+        } else if (v == btnSwichePlayer) {
             // Handle clicks for btnSwichePlayer
-        } else if ( v == btnExit ) {
+        } else if (v == btnExit) {
             finish();
             // Handle clicks for btnExit
-        } else if ( v == btnPre ) {
+        } else if (v == btnPre) {
+            setPreVideo();
             // Handle clicks for btnPre
-        } else if ( v == btnStartPause ) {
+        } else if (v == btnStartPause) {
             // Handle clicks for btnStartPause
             startAndPause();
-        } else if ( v == btnNext ) {
+        } else if (v == btnNext) {
             // Handle clicks for btnNext
-        } else if ( v == btnSwichScreen ) {
+            setNextVideo();
+        } else if (v == btnSwichScreen) {
             // Handle clicks for btnSwichScreen
         }
     }
 
+
+
     private void startAndPause() {
-        if(videoview.isPlaying()) {
+        if (videoview.isPlaying()) {
             videoview.pause();
             btnStartPause.setBackgroundResource(R.drawable.btn_start_selector);
-        }else{
+        } else {
             videoview.start();
             btnStartPause.setBackgroundResource(R.drawable.btn_pause_selector);
         }
@@ -161,11 +165,11 @@ public class SystemVideoPlayer extends Activity implements View.OnClickListener,
     }
 
     private void initData() {
-        utils=new Utils();
-        IntentFilter filter=new IntentFilter();
+        utils = new Utils();
+        IntentFilter filter = new IntentFilter();
         filter.addAction(Intent.ACTION_BATTERY_CHANGED);
-        receive=new MyBatterReceiver();
-        registerReceiver(receive,filter);
+        receive = new MyBatterReceiver();
+        registerReceiver(receive, filter);
     }
 
     private void setLinstener() {
@@ -173,30 +177,30 @@ public class SystemVideoPlayer extends Activity implements View.OnClickListener,
         videoview.setOnErrorListener(new MyOnErrorListener());
         videoview.setOnPreparedListener(new MyOnPreparedListener());
         seekbarVideo.setOnSeekBarChangeListener(this);
-       // videoview.setMediaController(new MediaController(this));
+        // videoview.setMediaController(new MediaController(this));
     }
 
     private void setData() {
-        if(video!=null&&video.size()>0) {
-           MediaItem mediaItem=video.get(position);
+        if (video != null && video.size() > 0) {
+            MediaItem mediaItem = video.get(position);
             videoview.setVideoPath(mediaItem.getData());
             tvName.setText(mediaItem.getName());
-        }else{
+        } else {
             videoview.setVideoURI(uri);
             tvName.setText(uri.toString());
         }
-
+        checkButtonStatus();
     }
 
     private void getData() {
         uri = getIntent().getData();
-        video= (ArrayList<MediaItem>) getIntent().getSerializableExtra("videolist");
-        position=getIntent().getIntExtra("position",0);
+        video = (ArrayList<MediaItem>) getIntent().getSerializableExtra("videolist");
+        position = getIntent().getIntExtra("position", 0);
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-        if(fromUser) {
+        if (fromUser) {
             seekBar.setProgress(progress);
             videoview.seekTo(progress);
         }
@@ -215,7 +219,7 @@ public class SystemVideoPlayer extends Activity implements View.OnClickListener,
     private class MyOnPreparedListener implements MediaPlayer.OnPreparedListener {
         @Override
         public void onPrepared(MediaPlayer mp) {
-            int duration=videoview.getDuration();
+            int duration = videoview.getDuration();
             tvDuration.setText(utils.stringForTime(duration));
             seekbarVideo.setMax(duration);
             handler.sendEmptyMessage(PROGRESS);
@@ -234,16 +238,85 @@ public class SystemVideoPlayer extends Activity implements View.OnClickListener,
     private class MyOnCompletionListener implements MediaPlayer.OnCompletionListener {
         @Override
         public void onCompletion(MediaPlayer mp) {
-            Toast.makeText(SystemVideoPlayer.this, "播放完毕", Toast.LENGTH_SHORT).show();
+           /* Toast.makeText(SystemVideoPlayer.this, "播放完毕", Toast.LENGTH_SHORT).show();
+            finish();*/
+            setNextVideo();
+        }
+    }
+    private void setPreVideo() {
+        if (video != null && video.size() > 0) {
+            position--;
+            if (position >0) {
+                MediaItem media = video.get(position);
+                videoview.setVideoPath(media.getData());
+                tvName.setText(media.getName());
+                checkButtonStatus();
+
+            } else {
+                position = 0;
+                finish();
+            }
+
+        } else if(uri!=null) {
             finish();
         }
     }
 
+    private void setNextVideo() {
+        if (video != null && video.size() > 0) {
+            position++;
+            if (position < video.size()) {
+                MediaItem media = video.get(position);
+                videoview.setVideoPath(media.getData());
+                tvName.setText(media.getName());
+                checkButtonStatus();
+
+            } else {
+                position = video.size()-1;
+                finish();
+            }
+
+        } else if(uri!=null) {
+            finish();
+        }
+    }
+
+    private void checkButtonStatus() {
+        setButtonEnable(true);
+        if (video != null && video.size() > 0) {
+           if(position==0) {
+               btnPre.setEnabled(false);
+               btnPre.setBackgroundResource(R.drawable.btn_pre_gray);
+           }
+            if(position==video.size()-1) {
+                btnNext.setEnabled(false);
+                btnNext.setBackgroundResource(R.drawable.btn_next_gray);
+            }
+
+        } else if(uri!=null) {
+            setButtonEnable(false);
+        }
+
+    }
+
+    private void setButtonEnable(boolean b) {
+        if(b) {
+            btnPre.setBackgroundResource(R.drawable.btn_pre_selector);
+            btnNext.setBackgroundResource(R.drawable.btn_next_selector);
+        }else{
+            btnPre.setBackgroundResource(R.drawable.btn_pre_gray);
+            btnNext.setBackgroundResource(R.drawable.btn_next_gray);
+        }
+        btnPre.setEnabled(b);
+        btnNext.setEnabled(b);
+
+    }
+
     @Override
     protected void onDestroy() {
-        if(handler!=null) {
+        if (handler != null) {
             handler.removeCallbacksAndMessages(null);
-            handler=null;
+            handler = null;
         }
         super.onDestroy();
     }
@@ -252,29 +325,29 @@ public class SystemVideoPlayer extends Activity implements View.OnClickListener,
 
         @Override
         public void onReceive(Context context, Intent intent) {
-            int level=intent.getIntExtra("level",0);
+            int level = intent.getIntExtra("level", 0);
             setBattery(level);
         }
     }
 
     private void setBattery(int level) {
-        if(level<=0) {
+        if (level <= 0) {
             ivBattery.setImageResource(R.drawable.ic_battery_0);
-        }else if(level<=10) {
+        } else if (level <= 10) {
             ivBattery.setImageResource(R.drawable.ic_battery_10);
-        }else if(level<=20) {
+        } else if (level <= 20) {
             ivBattery.setImageResource(R.drawable.ic_battery_20);
-        }else if(level<=40) {
+        } else if (level <= 40) {
             ivBattery.setImageResource(R.drawable.ic_battery_40);
-        }else if(level<=60) {
+        } else if (level <= 60) {
             ivBattery.setImageResource(R.drawable.ic_battery_60);
-        }else if(level<=80) {
+        } else if (level <= 80) {
             ivBattery.setImageResource(R.drawable.ic_battery_80);
-        }else if(level<=100) {
+        } else if (level <= 100) {
             ivBattery.setImageResource(R.drawable.ic_battery_100);
-        }else{
+        } else {
             ivBattery.setImageResource(R.drawable.ic_battery_100);
         }
-        
+
     }
 }
